2616beca067b7b94e1535aeade0b3e5ea02bcbe4
Tony Lin <tonylin@Tonys-MacBook-Pro-2.local>
2025-04-17 19:17:18 -0700

    i love austin

--- ee354_GCD.v
+++ ee354_GCD.v
@@ -1,117 +1,97 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Author:			Shideh Shahidi, Bilal Zafar, Gandhi Puvvada
-// Create Date:   02/25/08, 10/13/08
-// File Name:		ee354_GCD.v 
-// Description: 
-//
-//
-// Revision: 		2.1
-// Additional Comments:  
-// 10/13/2008 SCEN has been added by Gandhi
-//  3/1/2010  Signal names are changed in line with the divider_verilog design
-//           `define is replaced by localparam construct
-//  02/24/2020 Nexys-3 to Nexys-4 conversion done by Yue (Julien) Niu and reviewed by Gandhi
-//////////////////////////////////////////////////////////////////////////////////
+
+    module mastermind_core(Clk, Reset, correct_answer, current_color, confirm_color, check_guess, BtnL, BtnR, index, guess_num, current_guess, q_Start, q_Input, q_Check, q_DoneC, q_DoneNC);
 
 
-module ee354_GCD(Clk, SCEN, Reset, Start, Ack, Ain, Bin, A, B, AB_GCD, i_count, q_I, q_Sub, q_Mult, q_Done);
+        /*  INPUTS */
+        input Clk, Reset;
+        input [11:0] correct_answer; // 4 colors of 3 bits each
+        input [2:0] current_color; // color input from switches
 
+        // Buttons
+        input confirm_color;
+        input check_guess;
+        input BtnL, BtnR;
 
-	/*  INPUTS */
-	input	Clk, SCEN, Reset, Start, Ack;
-	input [7:0] Ain;
-	input [7:0] Bin;
-	
-	// i_count is a count of number of factors of 2	. We do not need an 8-bit counter. 
-	// However, in-line with other variables, this has been made an 8-bit item.
-	/*  OUTPUTS */
-	// store the two inputs Ain and Bin in A and B . These (A, B) are also continuously output to the higher module. along with the AB_GCD
-	output reg [7:0] A, B, AB_GCD, i_count;		// the result of the operation: GCD of the two numbers
-	// store current state
-	output q_I, q_Sub, q_Mult, q_Done;
-	reg [3:0] state;	
-	assign {q_Done, q_Mult, q_Sub, q_I} = state;
-		
-	localparam 	
-	I = 4'b0001, SUB = 4'b0010, MULT = 4'b0100, DONE = 4'b1000, UNK = 4'bXXXX;
-	
-	// NSL AND SM
-	always @ (posedge Clk, posedge Reset)
-	begin 
-		if(Reset) 
-		  begin
-			state <= I;
-			i_count <= 8'bx;  	// ****** TODO ******
-			A <= 8'bx;		  	// complete the 3 lines
-			B <= 8'bx;
-			AB_GCD <= 8'bx;			
-		  end
-		else				// ****** TODO ****** complete several parts
-				case(state)	
-					I:
-					begin
-						// state transfers
-						if (Start) state <= SUB;
-						// data transfers
-						i_count <= 0;
-						A <= Ain;
-						B <= Bin;
-						AB_GCD <= 0;
-					end		
-					SUB: 
-		               if (SCEN) //  This causes single-stepping the SUB state
-						begin		
-							// state transfers
-							if (A == B) state <= (i_count == 0) ?  DONE: MULT  ;
-							// data transfers
-							if (A == B) AB_GCD <= A;		
-							if (A < B)
-							  begin
-								// swap A and B
-								A <= B;
-								B <= A;
- 
-							  end
-							else						// if (A > B)
-							  begin	
-								if (!A[0]&!B[0])
-									begin
-										i_count <= i_count + 1'b1;
-										A <= A/2;
-										B <= B/2;
-									end
-								else if (A[0] & !B[0]) // A is odd b is even
-									begin
-										B <= B/2;
-									end
-								else if (!A[0]&B[0]) // 
-									begin
-									A <= A/2;
-									end
-								else
-									begin
-									A <= A-B;
-									end
-							  end
-						end
-					MULT:
-					  if (SCEN) // This causes single-stepping the MULT state
-						begin
-							// state transfers
-							state <= (i_count == 1) ?  DONE: MULT  ;
-							// data transfers
-							AB_GCD <= AB_GCD * 2;
-							i_count <= i_count - 1;
-						end
-					DONE:
-						if (Ack)	state <= I;
-						
-					default:		
-						state <= UNK;
-				endcase
-	end
-		
-	// OFL
-	// no combinational output signals
-	
-endmodule
+        // OUTPUTS
+        output reg[1:0] index; // Position of current color cursor
+        output reg[2:0] guess_num; // Guess number
+        output reg[11:0] current_guess;
+        
+        // States
+        output q_Start, q_Input, q_Check, q_DoneC, q_DoneNC;
+        reg [4:0] state;    
+        assign {q_Start, q_Input, q_Check, q_DoneC, q_DoneNC} = state;
+
+        reg [11:0] target;
+
+        wire all_filled =
+            (current_guess[ 2:0] != 3'd0) &&
+            (current_guess[ 5:3] != 3'd0) &&
+            (current_guess[ 8:6] != 3'd0) &&
+            (current_guess[11:9] != 3'd0);
+            
+        localparam 
+        START   = 5'b10000,
+        INPUT   = 5'b01000,
+        CHECK   = 5'b00100,
+        DONEC   = 5'b00010,
+        DONENC  = 5'b00001;
+
+        
+        // NSL AND SM
+        always @ (posedge Clk, posedge Reset)
+        begin 
+            if(Reset) 
+            begin
+                state <= START;
+                index <= 2'b0;  
+                guess_num <= 3'b0;
+                target <= 12'b0;
+                current_guess <= 12'b0;
+            end
+            else            
+                    case(state) 
+                        START:
+                        begin
+                            // state transfers
+                            state <= INPUT;
+                            // data transfers
+                            index <= 2'b0;  
+                            guess_num <= 3'b0;
+                            target <= correct_answer;
+                        end     
+                        INPUT: 
+                            begin       
+                                // state transfers
+                                if (check_guess && all_filled)
+                                    state <= CHECK;
+                                // data transfers
+                                if (BtnR && (index != 2'b11))
+                                    index <= index + 1;
+                                if (BtnL && (index != 2'b00))
+                                    index <= index - 1;
+                                if (confirm_color)
+                                    current_guess[index*3 +: 3] <= current_color; // move to the index of index * 3 and input the next 3 bits
+
+                            end
+                        CHECK:
+                            begin
+                                // state transfers
+                                if (current_guess == target)
+                                    state <= DONEC;
+                                else if (guess_num == 3'b101)
+                                    state <= DONENC;
+                                else 
+                                    begin
+                                        state <= INPUT;
+                                        guess_num <= guess_num + 1;
+                                    end
+                                // data transfers
+                            end
+                        DONEC:
+                        DONENC: 
+                    endcase
+        end
+            
+    endmodule
+
