a1ef9afa7dd3569b7a168cc1ab06dd0c6a641daf
Austin Tsai <aytsai@usc.edu>
2025-04-28 10:47:50 -0700

    fixed the stupid xdc file

--- mastermind_top_A7.v
+++ mastermind_top_A7.v
@@ -1,31 +1,47 @@
-`timescale 1ns / 1ps
-
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:18:00 12/14/2017 
+// Design Name: 
+// Module Name:    vga_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+// Date: 04/04/2020
+// Author: Yue (Julien) Niu
+// Description: Port from NEXYS3 to NEXYS4
+//////////////////////////////////////////////////////////////////////////////////
 module ee354_mastermind_top_A7(
-    // Memory disable
-    output wire       QuadSpiFlashCS,
+	input ClkPort,
+	input BtnC,
+	input BtnU,
+	input BtnR,
+	input BtnL,
+	input BtnD,
+  input Sw5, Sw4, Sw3, Sw2, Sw1, Sw0,
+	//VGA signal
+	output hSync, vSync,
+	output [3:0] vgaR, vgaG, vgaB,
+	output Ld0, Ld1, Ld2, Ld3, Ld4, Ld5,
+  
+	//SSD signal 
+	output An0, An1, An2, An3, An4, An5, An6, An7,
+	output Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp,
+	
+	//output MemOE, MemWR, RamCS,
+	output  QuadSpiFlashCS
+	);
 
-    // Clock & reset
-    input  wire       ClkPort,
-    input  wire       BtnL, BtnU, BtnD, BtnR, BtnC,
-    input  wire       Sw5, Sw4, Sw3, Sw2, Sw1, Sw0,
-
-    // LEDs
-    output wire       Ld0, Ld1, Ld2, Ld3, Ld4, Ld5,
-
-    // Seven-segment
-    output wire       Cg, Cf, Ce, Cd, Cc, Cb, Ca, Dp,
-    output wire       An0, An1, An2, An3, An4, An5, An6, An7,
-
-    // VGA
-    output wire       hSync, vSync,
-    output wire [3:0] vgaR, vgaG, vgaB
-);
-
-  // Disable external memories
-  wire MemOE, MemWR, RamCS;
-  assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
-
-    // Clock buffering and reset: use direct port clock as system clock
+	// Clock buffering and reset: use direct port clock as system clock
   wire sys_clk = ClkPort;
   // debounced reset
   wire reset_db;
@@ -44,19 +60,12 @@ module ee354_mastermind_top_A7(
   wire q_Start, q_Input, q_Check, q_DoneC, q_DoneNC;
   reg  [11:0] correct_answer = 12'b001_001_001_001;
   reg  [2:0]  current_color;
-  wire [71:0] matrix_flat;
+  reg [71:0] matrix_flat;
 
-  reg [1:0] index;
+  wire [1:0] index;
   // Update current_color from switches when in INPUT
-  always @(posedge sys_clk, posedge db_reset) 
+  always @(posedge sys_clk) 
   begin
-	if (db_reset)
-	begin
-		current_color <= 3'b000; // reset color
-		current_guess <= 12'b000000000000; // reset guess
-		guessNumber <= 3'b000; // reset guess number
-
-	end
   	if (q_Input) begin
 		case ({Sw5,Sw4,Sw3,Sw2,Sw1,Sw0})
 		6'b000001: current_color <= 3'b001;
@@ -67,7 +76,7 @@ module ee354_mastermind_top_A7(
 		6'b100000: current_color <= 3'b110;
 		default:   current_color <= 3'b000;
 		endcase
-		matrix_flat[guessNumber*12 + (index+1)*3 - 1: guessNumber*12 + index*3] <= current_color;
+		matrix_flat[guessNumber * 12 + index * 3 +: 3] <= current_color;
 	end
   end
 
@@ -121,5 +130,7 @@ module ee354_mastermind_top_A7(
   // Drive LEDs and SSDs (example)
   assign {Ld5,Ld4,Ld3,Ld2,Ld1,Ld0} = {Sw5,Sw4,Sw3,Sw2,Sw1,Sw0};
   assign {An7,An6,An5,An4,An3,An2,An1,An0} = 8'b1111_1111;
+
+	
 
 endmodule
